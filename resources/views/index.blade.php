254fca56cfae1282aae7e8667d2497793500f6d3
FbSoldier <felipelimabastos1@gmail.com>
2024-02-08 12:00:22 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,107 @@
+@extends('padrao')
+@section('content')
+<h1 class=""  >
+    Bem-vindo!
+</h1>
+
+<div class = "">
+    <div class="card mb-3" style="background-color: #2b2b2b;">
+  <div class="card-body">
+
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="../imagens/crash-bandicoot-icon-11.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Crash Team Racing</h5>
+              <p class="card-text">  Crash Team Racing, chamado no Japão de Crash Bandicoot Racing é o quarto jogo da série Crash Bandicoot.
+            </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  <a href="/cadastrar" class="btn btn-warning">Add To Cart</a>
+  </div>
+
+    <div class="card mb-3" style="background-color: #2b2b2b;">
+  <div class="card-body">
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="../imagens/60032-crash-bash-logo.png.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Crash Bash</h5>
+              <p class="card-text">Numa espécie de templo flutuante no Hiperespaço, Aku Aku e Uka Uka discutem para ver quem é melhor: o bem ou o mal.
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  <a href="/cadastrar" class="btn btn-warning">Add To Cart</a>
+  </div>
+
+  <div class="card mb-3" style="background-color: #2b2b2b;">
+    <div class="card-body">
+      <div class="card mb-3" style="max-width: 540px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="../imagens/Irmão do Jorel Lockscreen.png" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Jorelas</h5>
+                <p class="card-text">  Quando um videogame misterioso cai do céu, o Irmão do Jorel fará de tudo para jogar, mesmo que isso o leve aos confins da galáxia! 
+
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <a href="/cadastrar" class="btn btn-success">Add To Cart</a>
+    </div>
+
+    <div class="card mb-3" style="background-color: #2b2b2b;">
+        <div class="card-body">
+          <div class="card mb-3" style="max-width: 540px;">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="../imagens/imagem_2024-02-08_092715807.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Minecraft</h5>
+                    <p class="card-text">Espanque árvores
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <a href="/cadastrar" class="btn btn-success">Add To Cart</a>
+        </div>
+        
+        <div class="card mb-3" style="background-color: #2b2b2b;">
+            <div class="card-body">
+              <div class="card mb-3" style="max-width: 540px;">
+                  <div class="row g-0">
+                    <div class="col-md-4">
+                      <img src="../imagens/9vm1hlltw6281.png" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">Crab Game</h5>
+                        <p class="card-text"> Lute contra carangueijos até a morte!
+        
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <a href="/cadastrar" class="btn btn-danger">Add To Cart</a>
+            </div>
+</div> 
+@endsection
---
+++ resources/views/padrao.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/padrao.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  
+    <title>Nuuvem</title>
+</head>
+<body class="text-white"  style="background-color: #2b2b2b;">
+<!--Inicio Navbar -->
+
+<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../imagens/crash-bandicoot-icon-11.jpg" alt="Logo" width="45" height="45" class="d-inline-block align-text-top">
+      Nuuvem
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/cadastrar">Cadastro</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/todosContato">Buscar</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- fim Navbar -->
+
+@yield('content')
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+<!--  Fotter-->
+<section class="">
+  <!-- Footer -->
+  <footer class="text-center text-white" style="background-color: #111111;">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: CTA -->
+      <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+          <span class="me-3">Register for free</span>
+          <a href="/cadastrar" class="btn btn-secondary">Sign up!</a>
+            
+          </button>
+        </p>
+      </section>
+      <!-- Section: CTA -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2024 Copyright:
+      <a class="text-white" href="http://127.0.0.1:8000">Nuuvem.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</section>
+
+
+
+</body>
+</html>
